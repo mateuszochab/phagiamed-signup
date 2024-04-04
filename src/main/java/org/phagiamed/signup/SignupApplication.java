1b164b299bcdac03c553fae72bf06b35cfbbdfbe
ochab.mat@gmail.com <ochab.mat@gmail.com>
2024-04-04 22:09:05 +0200

    test commit

--- src/main/java/org/phagiamed/signup/SignupApplication.java
+++ src/main/java/org/phagiamed/signup/SignupApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SignupApplication {
-//comment test
+//comment test 1
 	public static void main(String[] args) {
 		SpringApplication.run(SignupApplication.class, args);
 	}
