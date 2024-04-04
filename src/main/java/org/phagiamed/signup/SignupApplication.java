21312085db21da992357b42b86fa7f3d21535464
ochab.mat@gmail.com <ochab.mat@gmail.com>
2024-04-04 22:15:16 +0200

    pipeline test

--- src/main/java/org/phagiamed/signup/SignupApplication.java
+++ src/main/java/org/phagiamed/signup/SignupApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SignupApplication {
-//comment test 2
+//comment test 3
 	public static void main(String[] args) {
 		SpringApplication.run(SignupApplication.class, args);
 	}
